15f65ed260f593bbe437f1c3f3d478b19abc4585
Turbobot <turbobot@vercel.com>
2024-06-10 15:48:49 +0900

    feat(create-turbo): apply official-starter transform

--- apps/blog/.eslintrc.js
+++ apps/blog/.eslintrc.js
@@ -1,6 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/next.js"],
+  extends: ["@repo/eslint-config/remix.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
